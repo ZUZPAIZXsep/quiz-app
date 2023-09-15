9d87311a1d8f3b8cc4d511e94ea8c80914d8b98d
ZUZPAIZXsep <s6303051613092@email.kmutnb.ac.th>
2023-09-15 13:48:59 +0700

    Add component and data
    add Menu.js Quiz.js Score.js and QuestionData.js

---
+++ src/component/Quiz.js
@@ -0,0 +1,38 @@
+import { useEffect,useState } from "react";
+import QuestionsData from "../data/QuestionsData";
+
+const Quiz = ()=>{
+    //console.log(QuestionsData);
+    const [current, setCurrent] = useState(0)
+    const [selectChoice, setSelectChoice] = useState("")
+
+    useEffect(()=>{
+        checkAnswer()
+    },[selectChoice])
+
+    const checkAnswer=()=>{
+        if(selectChoice!==""){
+            if(selectChoice===QuestionsData[current].answer){
+                console.log("ตอบถูกได้คะแนน")
+            }else{
+                console.log("ตอบผิดไม่ได้คะแนน T_T")
+            }
+        }
+    }
+
+
+    // return(
+    //     <div className="quiz">
+    //         <h1>{QuestionsData[current].question}</h1>
+    //         <div className="choices">
+    //             <button onClick={()=>setSelectChoice("A")}>{QuestionsData[current].A}</button>
+    //             <button onClick={()=>setSelectChoice("B")}>{QuestionsData[current].B}</button>
+    //             <button onClick={()=>setSelectChoice("C")}>{QuestionsData[current].C}</button>
+    //             <button onClick={()=>setSelectChoice("D")}>{QuestionsData[current].D}</button>
+    //         </div>
+    //         <p>{`${current+1} / ${QuestionsData.length}`}</p>
+    //     </div>
+    // )
+}
+
+export default Quiz;
